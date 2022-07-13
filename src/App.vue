e6e88698ce25341754014b8fcced7339ddbc86e5
Tocco17 <fede.ruocco@gmail.com>
2022-07-13 08:28:47 +0200

    Inizializzazione progetto

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  data() {
+    return {
+      forms: []
+    }
+  },
+
+  methods: {
+
+  }
+
+})
+
+</script>
+
+<template>
+  <h1>Hello App!</h1>
+  <p>
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/login">Go to Login</router-link>
+  </p>
+
+
+  <router-view />
+</template>
+
+<style>
+</style>
