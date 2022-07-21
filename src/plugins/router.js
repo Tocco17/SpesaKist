c0e49a48410224608a662e5ef1e98f1d8449c086
Tocco17 <fede.ruocco@gmail.com>
2022-07-21 08:51:45 +0200

    Added two pages

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,12 +2,16 @@ import { createRouter, createWebHistory } from "vue-router"
 import { useAuthStore } from "../stores/authStore"
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Articles from '../views/Articles.vue'
+import Recipies from '../views/Recipies.vue'
 
 
 // We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: Home, meta: { requiresAuth: true } },
-  { path: '/login', component: Login },
+    { path: '/', component: Home, meta: { requiresAuth: true } },
+    { path: '/Login', component: Login },
+    { path: '/Articles', component: Articles, meta: { requiresAuth: true } },
+    { path: '/Recipies', component: Recipies, meta: { requiresAuth: true } },
 ]
 
 
@@ -15,24 +19,23 @@ const routes = [
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(),
-  routes, // short for `routes: routes`
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
 })
 
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore()
+    const authStore = useAuthStore()
 
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (authStore.isLogged) {
-      next()
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (authStore.isLogged) {
+            next()
+        } else {
+            next('/Login')
+        }
     } else {
-      next('/login')
+        next()
     }
-  } else {
-    next()
-  }
-}
-)
+})
 
 export default router
