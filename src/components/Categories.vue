1057a377b7cb2275fdbc1645c9fa6657dc7ccb95
Tocco17 <fede.ruocco@gmail.com>
2022-07-28 17:16:41 +0200

    Managing of categories list

---
+++ src/components/Categories.vue
@@ -0,0 +1,34 @@
+<script>
+import { useArticlesStore } from "../stores/articlesStore";
+
+export default {
+    name: 'categories',
+    data() {
+        return {
+            categories: this.articlesStore.getCategories
+        }
+    },
+    setup() {
+        const articlesStore = useArticlesStore();
+        return {
+            articlesStore
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="bg-gray-500 flex flex-col flex-wrap justify-start ml-50 mr-50 p-10 rounded-lg">
+        <!--//HEADER-->
+        <h2 class="">Categories</h2>
+
+        <!--//LIST-->
+        <ul class="flex flex-col">
+            <li v-for="(category, index) in categories">
+                <input :id="`category-${index}`" type="checkbox" value="true" :checked="category.checked"
+                    @click="category.checked = !category.checked">
+                <label :for="`category-${index}`">{{ category.value }}</label>
+            </li>
+        </ul>
+    </div>
+</template>
---
+++ src/views/Articles.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from "vue";
+import { useArticlesStore } from "../stores/articlesStore";
+import categories from "../components/Categories.vue"
+
+export default defineComponent({
+    data() {
+        return {
+            articles: this.articlesStore.articles,
+        }
+    },
+    components: {
+        categories,
+    },
+    setup() {
+        const articlesStore = useArticlesStore();
+        return {
+            articlesStore
+        }
+    },
+    methods: {
+    },
+    computed: {
+        // articlesToView() {
+        //     const selectedCategories = categories.filter(x => x.checked)
+        //     return this.articles.filter(x => x.categories.some(c => selectedCategories.includes(c)))
+        // }
+    },
+})
+</script>
+
+<template>
+    <categories />
+</template>
