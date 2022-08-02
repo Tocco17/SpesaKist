02d679699849a6c77ea3c23c8873db0bfa4de600
Tocco17 <fede.ruocco@gmail.com>
2022-08-02 16:05:52 +0200

    Added creation of a list

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -6,6 +6,11 @@ import articles from "../components/Articles.vue"
 
 export default defineComponent({
     data() {
+        return {
+            articleImgSrc: '',
+            toCheck: false,
+            listName: '',
+        }
     },
     components: {
         categories,
@@ -18,6 +23,16 @@ export default defineComponent({
         // }
     },
     methods: {
+        articleHover(article) {
+            this.articleImgSrc = '/src/assets/' + article.image
+        },
+        saveNewList() {
+            console.log('Articles.vue/saveNewList =>')
+
+            console.log('this.listName', this.listName)
+
+            console.log('<= Articles.vue/saveNewList')
+        }
     },
     computed: {
     },
@@ -25,7 +40,31 @@ export default defineComponent({
 </script>
 
 <template>
-    <categories />
+    <div class="flex flex-row-reverse justify-between items-center">
+        <div v-if="toCheck" class="flex flex-row justify-between items-center order-2">
+            <h2 class="text-2xl mr-2">New List</h2>
 
-    <articles />
+            <div class="flex flex-row items-center mr-2">
+                <label for="newListInput" class="mr-1">List Name:</label>
+                <input id="newListInput" class="rounded" type="text" placeholder="Insert a name list"
+                    :value="listName" />
+            </div>
+
+            <button class="bg-green-800 rounded-md text-xl text-white p-2" @click="saveNewList">Save</button>
+        </div>
+
+        <button class="bg-green-800 rounded-full text-3xl text-white text-justify order-1 p-5"
+            @click="toCheck = !toCheck">
+            {{ toCheck ? '-' : '+' }}
+        </button>
+    </div>
+    <div class="flex flex-row justify-start items-start">
+        <categories class="m-1" />
+
+        <articles class="m-1" @article-hover="articleHover" :toCheck="toCheck" />
+
+        <div>
+            <v-img :src="articleImgSrc"></v-img>
+        </div>
+    </div>
 </template>
